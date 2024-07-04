dd01964ec8fd4d31f3d74418a258cd6761d1b93b
Mohd Zaid <mohdzaidali75@gmail.com>
2024-07-05 00:57:42 +0530

    test

--- src/pages/user/unprotectedRoutes/home/components/faq/Faq.jsx
+++ src/pages/user/unprotectedRoutes/home/components/faq/Faq.jsx
@@ -61,54 +61,6 @@ const Faq = () => {
             numquam ipsam iure consectetur voluptatum?
           </p>
         </Accordion.Panel>
-        <Accordion.Panel header="Question 1">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
-            nesciunt accusamus expedita iusto minus aut praesentium modi? Cum
-            officiis ad recusandae soluta numquam modi similique, deserunt
-            assumenda blanditiis rem quasi fugiat amet natus. Aspernatur placeat
-            similique totam minus sit iusto corrupti ullam voluptate assumenda
-            eum, commodi ea ratione maiores fugiat, alias beatae excepturi
-            dignissimos. Ut obcaecati iste provident ratione id quos facere quae
-            tenetur corrupti quidem est fuga nihil maiores vitae odit fugiat
-            nobis consectetur aspernatur, nesciunt magni voluptatem eligendi!
-            Rem, aliquid placeat dolor odio fugiat voluptatem aperiam.
-            Dignissimos beatae consequuntur molestias. Temporibus ut quis
-            numquam ipsam iure consectetur voluptatum?
-          </p>
-        </Accordion.Panel>
-        <Accordion.Panel header="Question 2">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
-            nesciunt accusamus expedita iusto minus aut praesentium modi? Cum
-            officiis ad recusandae soluta numquam modi similique, deserunt
-            assumenda blanditiis rem quasi fugiat amet natus. Aspernatur placeat
-            similique totam minus sit iusto corrupti ullam voluptate assumenda
-            eum, commodi ea ratione maiores fugiat, alias beatae excepturi
-            dignissimos. Ut obcaecati iste provident ratione id quos facere quae
-            tenetur corrupti quidem est fuga nihil maiores vitae odit fugiat
-            nobis consectetur aspernatur, nesciunt magni voluptatem eligendi!
-            Rem, aliquid placeat dolor odio fugiat voluptatem aperiam.
-            Dignissimos beatae consequuntur molestias. Temporibus ut quis
-            numquam ipsam iure consectetur voluptatum?
-          </p>
-        </Accordion.Panel>
-        <Accordion.Panel header="Question 3">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
-            nesciunt accusamus expedita iusto minus aut praesentium modi? Cum
-            officiis ad recusandae soluta numquam modi similique, deserunt
-            assumenda blanditiis rem quasi fugiat amet natus. Aspernatur placeat
-            similique totam minus sit iusto corrupti ullam voluptate assumenda
-            eum, commodi ea ratione maiores fugiat, alias beatae excepturi
-            dignissimos. Ut obcaecati iste provident ratione id quos facere quae
-            tenetur corrupti quidem est fuga nihil maiores vitae odit fugiat
-            nobis consectetur aspernatur, nesciunt magni voluptatem eligendi!
-            Rem, aliquid placeat dolor odio fugiat voluptatem aperiam.
-            Dignissimos beatae consequuntur molestias. Temporibus ut quis
-            numquam ipsam iure consectetur voluptatum?
-          </p>
-        </Accordion.Panel>
       </Accordion>
     </section>
   );
