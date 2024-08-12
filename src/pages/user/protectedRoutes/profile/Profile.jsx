ec0071ee922be59d7720e096640db16bf04b32f1
DELL <syedabbas83778@gmail.com>
2024-08-11 21:47:11 -0700

    integrated profile image api

--- src/pages/user/protectedRoutes/profile/Profile.jsx
+++ src/pages/user/protectedRoutes/profile/Profile.jsx
@@ -1,8 +1,36 @@
 import "./profile.scss";
-
 import profileImg from "@/assets/profile.webp";
 
+//Redux
+import {
+  useUserGetProfileQuery,
+  useUserProfilePicUploadMutation,
+} from "@/redux/slice/user/api/authUserApiSlice";
+
 const Profile = () => {
+  const { data: profileDetails, isLoading: profileLoading } =
+    useUserGetProfileQuery();
+
+  const [imageUpload] = useUserProfilePicUploadMutation();
+  const handleFileChange = async (event) => {
+    const formsData = new FormData();
+    formsData.append("image", event.target.files[0]);
+    console.log(event.target.files.buffer);
+    const res = await imageUpload({ formsData });
+  };
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    convertToBase64(file);
+  };
+
+  const convertToBase64 = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = async () => {
+      const res = await imageUpload(reader.result);
+    };
+  };
   return (
     <div className="profile">
       <div className="content">
@@ -10,20 +38,35 @@ const Profile = () => {
       </div>
       <div className="form-container">
         <div className="form-container-left">
-          <img src={profileImg} alt="" />
+          <input
+            type="file"
+            className="changeImage"
+            onChange={handleImageUpload}
+          />
+          <img
+            src={
+              profileDetails?.data?.profilePic
+                ??profileImg
+            }
+            alt=""
+          />
         </div>
         <div className="form-container-right">
           <div>
             <label htmlFor="">Name : </label>
-            <input type="text" />
+            <input
+              type="text"
+              value={profileDetails?.data?.fullName}
+              readOnly
+            />
           </div>
           <div>
             <label htmlFor="">Email : </label>
-            <input type="text" />
+            <input type="text" value={profileDetails?.data?.email} readOnly />
           </div>
           <div>
             <label htmlFor="">Phone : </label>
-            <input type="text" />
+            <input type="text" value={profileDetails?.data?.phone} readOnly />
           </div>
         </div>
       </div>
