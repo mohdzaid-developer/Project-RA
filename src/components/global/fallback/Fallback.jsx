8c77e4b89977ba260a8e89290954d51fd0f218d0
Mohd Zaid <mohdzaidali75@gmail.com>
2024-08-18 04:05:19 +0530

    test

--- src/components/global/fallback/Fallback.jsx
+++ src/components/global/fallback/Fallback.jsx
@@ -5,6 +5,7 @@ import pageNotFound from "@/assets/not-found.png";
 
 //Components
 import UserNavbar from "@/components/user/userNavbar/UserNavbar";
+import Footer from "@/components/user/footer/Footer";
 
 const Fallback = () => {
   return (
@@ -14,6 +15,7 @@ const Fallback = () => {
         <h2>Page Not Found!</h2>
         <img src={pageNotFound} alt="" />
       </div>
+      <Footer />
     </>
   );
 };
