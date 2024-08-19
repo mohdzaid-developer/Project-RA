cbd8221ea74dd57df7e0b84cbbbfe58510f0045a
Mohd Zaid <mohdzaidali75@gmail.com>
2024-08-19 16:26:20 +0530

    test

--- src/pages/user/unprotectedRoutes/home/components/introduction/Introduction.jsx
+++ src/pages/user/unprotectedRoutes/home/components/introduction/Introduction.jsx
@@ -5,7 +5,7 @@ import ScrollText from "./scrollText/ScrollText";
 
 const Introduction = () => {
   const content = [
-    "At Project RA, Travel Agency, we believe that travel is not just about reaching a destination, but",
+    "At Come fly with me, Travel Agency, we believe that travel is not just about reaching a destination, but",
     "about the journey and the memories you create along the way. With over a decade of experience",
     "in the travel industry, we specialize in crafting personalized travel experiences that cater to your",
     "unique preferences and interests.",
