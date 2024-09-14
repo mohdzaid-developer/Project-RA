818fee5b16a47b516798ffbd6ed0588c0376e0e4
Mohd Zaid <mohdzaidali75@gmail.com>
2024-09-14 18:39:35 +0530

    done testimonial section

--- src/utils/loader/Loader.jsx
+++ src/utils/loader/Loader.jsx
@@ -1,12 +1,12 @@
-import { Watch } from "react-loader-spinner";
+import { TailSpin } from "react-loader-spinner";
 
 const Loader = () => {
   return (
     <div>
-      <Watch
-        radius="35"
+      <TailSpin
+        radius="1"
         color="#fefae0"
-        ariaLabel="watch-loading"
+        ariaLabel="tail-spin-loading"
         wrapperStyle={{
           height: "100vh",
           display: "flex",
