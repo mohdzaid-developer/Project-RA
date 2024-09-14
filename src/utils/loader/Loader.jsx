287419349464f6115a087e924a82b5d7ad5e8f14
Mohd Zaid <mohdzaidali75@gmail.com>
2024-09-14 20:49:58 +0530

    test

--- src/utils/loader/Loader.jsx
+++ src/utils/loader/Loader.jsx
@@ -4,6 +4,8 @@ const Loader = () => {
   return (
     <div>
       <TailSpin
+        height="40"
+        width="40"
         radius="1"
         color="#fefae0"
         ariaLabel="tail-spin-loading"
