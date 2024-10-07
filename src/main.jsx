a393c9537f7e1bd64ab9490f7cfe85db580c3f6c
Mohd Zaid <mohdzaidali75@gmail.com>
2024-10-07 13:21:55 +0530

    test

--- src/main.jsx
+++ src/main.jsx
@@ -6,21 +6,19 @@ import "./index.scss";
 //Routing
 import { BrowserRouter } from "react-router-dom";
 
-//Util
-import ScrollToTop from "./utils/scrollToTop/ScrollToTop.jsx";
-
 //Redux
 import { Provider } from "react-redux";
 import { store } from "./redux/store/store.js";
+import ScrollToTop from "./utils/scrollToTop/ScrollToTop.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ScrollToTop>
-        <Provider store={store}>
+      <Provider store={store}>
+        <ScrollToTop>
           <App />
-        </Provider>
-      </ScrollToTop>
+        </ScrollToTop>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
